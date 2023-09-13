1339ca1448d902aff369caa7106f8f5f1d9b85ea
AchrafbrekiDSI2022 <brikiachraf@DJERBA.r-iset.tn>
2023-09-13 22:35:45 +0200

    configuer le connexion avec Open weather Map

---
+++ weather-App/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather-map27.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName : 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue : 'open-weather-map27.p.rapidapi.com',
+  XRapidAPIKeyHeaderName : 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '33d140542emsh2327e42300d52dep12b10ajsn884842a25e66'
+};
---
+++ weather-App/src/app/services/weather.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from 'environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http:HttpClient) {
+
+   }
+   getWeatherData(cityName: string){
+    this.http.get(environment.weatherApiBaseUrl,{
+      headers: new HttpHeaders()
+      .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+      .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue),
+      params: new HttpParams()
+      .set('q', cityName)
+      .set('units', 'metric')
+      .set('mode', 'json')
+    })
+   }
+}
